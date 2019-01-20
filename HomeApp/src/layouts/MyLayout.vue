9fcd237c31f558b88c0db60e0f9854cdb14c0c77
ThaGary <garyanderson31591@gmail.com>
2019-01-20 13:45:19 -0700

    setting up quasar/vue framework

---
+++ HomeApp/src/layouts/MyLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-layout-header>
+      <q-toolbar
+        color="amber-8"
+      >
+
+        <q-toolbar-title>
+          HomeApp
+          <div slot="subtitle">Capstone</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ HomeApp/src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Capstone logo" src="~assets/capstonelogo.svg">
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
